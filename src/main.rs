d5ee4d587b561f9b84c855a822f52ca3cc887924
anima-libera <anim.libera@gmail.com>
2023-08-13 04:05:58 +0200

    Added a "card", removed tmp files

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,59 @@ use ggez::conf::WindowSetup;
 use ggez::event;
 use ggez::glam::*;
 use ggez::graphics::DrawParam;
-use ggez::graphics::{self, Color, Image};
+use ggez::graphics::{self, Canvas, Color, Image, Rect};
 use ggez::{Context, GameResult};
 
+enum CardSpec {
+	Fwog,
+	DragonFly,
+}
+
+impl CardSpec {
+	fn draw(
+		&self,
+		ctx: &mut Context,
+		canvas: &mut Canvas,
+		spritesheet: &Image,
+		dst: Vec2,
+	) -> GameResult {
+		let rectangle = graphics::Mesh::new_rectangle(
+			ctx,
+			graphics::DrawMode::stroke(3.0),
+			Rect::new(dst.x, dst.y, 200.0, 250.0),
+			Color::WHITE,
+		)?;
+		canvas.draw(&rectangle, Vec2::new(0.0, 0.0));
+
+		let (name, sprite) = match self {
+			CardSpec::Fwog => ("fwog", Rect::new(0.0, 0.2, 0.5, 0.4)),
+			CardSpec::DragonFly => ("dragon fly", Rect::new(0.5, 0.12, 0.5, 0.45)),
+		};
+
+		canvas.draw(
+			graphics::Text::new(name).set_scale(26.0),
+			graphics::DrawParam::from(Vec2::new(dst.x + 10.0, dst.y + 10.0)).color(Color::WHITE),
+		);
+		canvas.draw(
+			spritesheet,
+			DrawParam::default()
+				.dest(Vec2::new(dst.x + 10.0, dst.y + 30.0 + 10.0))
+				.scale(Vec2::new(0.1, 0.1))
+				.src(sprite),
+		);
+
+		Ok(())
+	}
+}
+
 struct Game {
-	frog_image: Image,
+	spritesheet: Image,
 }
 
 impl Game {
 	fn new(ctx: &Context) -> GameResult<Game> {
 		Ok(Game {
-			frog_image: Image::from_bytes(ctx, include_bytes!("../assets/spritesheet.png"))?,
+			spritesheet: Image::from_bytes(ctx, include_bytes!("../assets/spritesheet.png"))?,
 		})
 	}
 }
@@ -23,29 +65,20 @@ impl event::EventHandler<ggez::GameError> for Game {
 	}
 
 	fn draw(&mut self, ctx: &mut Context) -> GameResult {
-		let mut canvas =
-			graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+		let mut canvas = Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
-		let rectangle = graphics::Mesh::new_rectangle(
+		CardSpec::Fwog.draw(
 			ctx,
-			graphics::DrawMode::stroke(3.0),
-			graphics::Rect::new(100.0, 100.0, 200.0, 250.0),
-			Color::WHITE,
+			&mut canvas,
+			&self.spritesheet,
+			Vec2 { x: 100.0, y: 100.0 },
 		)?;
-		canvas.draw(&rectangle, Vec2::new(0.0, 0.0));
-
-		canvas.draw(
-			graphics::Text::new("fwog").set_scale(26.0),
-			graphics::DrawParam::from(Vec2::new(110.0, 110.0)).color(Color::WHITE),
-		);
-
-		canvas.draw(
-			&self.frog_image,
-			DrawParam::default()
-				.dest(Vec2::new(110.0, 140.0))
-				.scale(Vec2::new(0.1, 0.1))
-				.src(graphics::Rect::new(0.0, 0.2, 0.5, 0.4)),
-		);
+		CardSpec::DragonFly.draw(
+			ctx,
+			&mut canvas,
+			&self.spritesheet,
+			Vec2 { x: 320.0, y: 100.0 },
+		)?;
 
 		canvas.finish(ctx)?;
 		Ok(())
